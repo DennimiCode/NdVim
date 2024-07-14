cab8fd34b1204088abaef965b12bd6c6807879a1
DennimiCode <dennimi@duck.com>
2024-07-14 22:20:30 +0300

    Added: golines and goimport, removed: spell sources

--- lua/plugins/none_ls.lua
+++ lua/plugins/none_ls.lua
@@ -1,14 +1,15 @@
 local null_ls = require("null-ls")
 null_ls.setup({
   sources = {
-    null_ls.builtins.formatting.stylua,           -- Lua
-    null_ls.builtins.completion.spell,            -- Spelling
-    null_ls.builtins.formatting.clang_format,     -- C & C++, Objective-C and etc. in CLang
-    null_ls.builtins.formatting.gofmt,            -- Golang
-    null_ls.builtins.diagnostics.hadolint,
-    null_ls.builtins.formatting.sqlfmt,           -- SQL formatter
+    null_ls.builtins.formatting.stylua,             -- Lua
+    null_ls.builtins.completion.luasnip,            -- LuaSnip - snippets support
+    null_ls.builtins.formatting.clang_format,       -- C & C++, Objective-C and etc. in CLang
+    null_ls.builtins.formatting.gofmt,              -- Golang
+    null_ls.builtins.formatting.goimports,          -- Golang auto import
+    null_ls.builtins.formatting.golines,            -- Golang convert long strings to short
+    null_ls.builtins.diagnostics.hadolint,          -- Dockerfile
+    null_ls.builtins.formatting.sqlfmt,             -- SQL formatter
     null_ls.builtins.formatting.google_java_format, -- Java formatter with Google rules
-    null_ls.builtins.formatting.csharpier,        -- C# formatter
+    null_ls.builtins.formatting.csharpier,          -- C# formatter
   },
-  null_ls.builtins.completion.luasnip,
 })
