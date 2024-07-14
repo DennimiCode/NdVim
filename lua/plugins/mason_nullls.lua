688e5de856176ea327c6f14b4820e999797cb387
DennimiCode <dennimi@duck.com>
2024-07-14 22:21:38 +0300

    Added golines and goimport automatic installation

--- lua/plugins/mason_nullls.lua
+++ lua/plugins/mason_nullls.lua
@@ -1,13 +1,14 @@
 require("mason-null-ls").setup({
   ensure_installed = {
-    "stylua",           -- Lua formatter
-    "clang_format",     -- C & C++ formatter
-    "csharpier",        -- C# formatter
+    "stylua",             -- Lua formatter
+    "clang_format",       -- C & C++ formatter
+    "csharpier",          -- C# formatter
     "google-java-format", -- Java formatter with Google rules
-    "sqlfmt",           -- SQL formatter
-    "hadolint",
-    "cspell",
-    "goimports", -- Organize imports for Golang
+    "sqlfmt",             -- SQL formatter
+    "hadolint",           -- Dockerfile
+    "cspell",             -- Code Spell Checker
+    "goimports",          -- Organize imports for Golang
+    "golines",            -- Golang convert long strings to short
   },
   automatic_installation = true,
 })
