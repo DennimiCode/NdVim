0b9e2f441f952b04c86f17733c8d7a74f015649c
DennimiCode <dennimi@duck.com>
2024-07-18 10:11:32 +0300

    Removed luasnip (not required) and golines extensions

--- lua/plugins/none_ls.lua
+++ lua/plugins/none_ls.lua
@@ -2,11 +2,9 @@ local null_ls = require("null-ls")
 null_ls.setup({
   sources = {
     null_ls.builtins.formatting.stylua,             -- Lua
-    null_ls.builtins.completion.luasnip,            -- LuaSnip - snippets support
     null_ls.builtins.formatting.clang_format,       -- C & C++, Objective-C and etc. in CLang
     null_ls.builtins.formatting.gofmt,              -- Golang
     null_ls.builtins.formatting.goimports,          -- Golang auto import
-    null_ls.builtins.formatting.golines,            -- Golang convert long strings to short
     null_ls.builtins.diagnostics.hadolint,          -- Dockerfile
     null_ls.builtins.formatting.sqlfmt,             -- SQL formatter
     null_ls.builtins.formatting.google_java_format, -- Java formatter with Google rules
