cbe43bd67df68c7b12513f26974b76aca5ba5704
DennimiCode <dennimi@duck.com>
2024-08-03 14:21:36 +0300

    Added lspsaga config

--- lua/plugins/lspsaga.lua
+++ lua/plugins/lspsaga.lua
@@ -1,1 +1,14 @@
-require("lspsaga").setup({})
+require("lspsaga").setup({
+  ui = {
+    devicon = true,
+    title = true,
+    code_action = "󰌵",
+    imp_sign = "",
+    actionfix = "",
+    expand = "",
+    collapse = "",
+  },
+  beacon = { enable = true },
+  symbol_in_winbar = { enable = true },
+  implement = { enable = true },
+})
