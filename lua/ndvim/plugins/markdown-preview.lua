20ff284b1bfa691e8b5f963db3359e8a83a96657
DennimiCode <dennimi@duck.com>
2024-10-16 10:12:39 +0300

    feat(new-md-preview-plugin): Change md preview plugin
    Changed markdown preview plugin from markdown-preview
    to Glow.

--- lua/ndvim/plugins/markdown-preview.lua
+++ lua/ndvim/plugins/markdown-preview.lua
@@ -1,12 +1,5 @@
 return {
-  "iamcco/markdown-preview.nvim",
-  cmd = { "MarkdownPreviewToggle", "MarkdownPreview", "MarkdownPreviewStop" },
-  build = "cd app && npm install",
-  init = function()
-    vim.g.mkdp_filetypes = { "markdown" }
-  end,
-  ft = { "markdown" },
-  config = function()
-    vim.cmd([[do FileType]])
-  end,
+  "ellisonleao/glow.nvim",
+  config = true,
+  cmd = "Glow",
 }
