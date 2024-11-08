54fb5449e457263fcc9848ff2600d0f966455cb1
DennimiCode <dennimi@duck.com>
2024-11-08 13:19:18 +0300

    feat(neotree): Add neotree custom config

--- lua/ndvim/plugins/neotree.lua
+++ lua/ndvim/plugins/neotree.lua
@@ -9,7 +9,45 @@ return {
   },
   config = function()
     vim.g.neo_tree_remove_legacy_commands = 1
-    local neotree = require("neo-tree")
-    neotree.setup({})
+    require("neo-tree").setup({
+      filesystem = {
+        filtered_items = {
+          visible = true,
+          hide_dotfiles = false,
+          hide_gitignored = false,
+        },
+      },
+      popup_border_style = "rounded",
+      enable_git_status = true,
+      enable_diagnostics = true,
+      open_files_do_not_replace_types = { "terminal", "trouble", "qf" },
+      sort_case_insensitive = false,
+      default_component_configs = {
+        modified = {
+          symbol = "[󰐕]",
+          highlight = "NeoTreeModified",
+        },
+        name = {
+          trailing_slash = false,
+          use_git_status_colors = true,
+          highlight = "NeoTreeFileName",
+        },
+        git_status = {
+          symbols = {
+            -- Change type
+            added = "󰐕",
+            modified = "",
+            deleted = "󰧧",
+            renamed = "",
+            -- Status type
+            untracked = "",
+            ignored = "",
+            unstaged = "󰄱",
+            staged = "",
+            conflict = "",
+          },
+        },
+      },
+    })
   end,
 }
