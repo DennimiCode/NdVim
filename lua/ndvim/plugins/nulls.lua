67aadf03942159d41977171091dc08afe139af1d
DennimiCode <dennimi@duck.com>
2024-12-10 13:04:51 +0300

    feat(none-ls): Add format on save and prettier support

--- lua/ndvim/plugins/nulls.lua
+++ lua/ndvim/plugins/nulls.lua
@@ -1,17 +1,33 @@
 return {
   "nvimtools/none-ls.nvim",
   config = function()
+    local augroup = vim.api.nvim_create_augroup("LspFormatting", {})
     local null_ls = require("null-ls")
+
     null_ls.setup({
+      on_attach = function(client, bufnr)
+        if client.supports_method("textDocument/formatting") then
+          vim.api.nvim_clear_autocmds({ group = augroup, buffer = bufnr })
+          vim.api.nvim_create_autocmd("BufWritePre", {
+            group = augroup,
+            buffer = bufnr,
+            callback = function()
+              -- vim.lsp.buf.formatting_sync()
+              vim.lsp.buf.format({ bufnr = bufnr })
+            end,
+          })
+        end
+      end,
       sources = {
-        null_ls.builtins.formatting.stylua,             -- Lua
-        null_ls.builtins.formatting.clang_format,       -- C & C++, Objective-C and etc. in CLang
-        null_ls.builtins.formatting.gofmt,              -- Golang
-        null_ls.builtins.formatting.goimports,          -- Golang auto import
-        null_ls.builtins.diagnostics.hadolint,          -- Dockerfile
-        null_ls.builtins.formatting.sqlfmt,             -- SQL formatter
+        null_ls.builtins.formatting.prettierd, -- Prettier (js, ts, jsx, tsx)
+        null_ls.builtins.formatting.stylua, -- Lua
+        null_ls.builtins.formatting.clang_format, -- C & C++, Objective-C and etc. in CLang
+        null_ls.builtins.formatting.gofmt, -- Golang
+        null_ls.builtins.formatting.goimports, -- Golang auto import
+        null_ls.builtins.diagnostics.hadolint, -- Dockerfile
+        null_ls.builtins.formatting.sqlfmt, -- SQL formatter
         null_ls.builtins.formatting.google_java_format, -- Java formatter with Google rules
-        null_ls.builtins.formatting.csharpier,          -- C# formatter
+        null_ls.builtins.formatting.csharpier, -- C# formatter
       },
     })
   end,
